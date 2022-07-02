a0f546dba920534a3fa8441402aa7dd4753d0467
Vismcad <viscmadsingh93@gmail.com>
2022-07-02 13:55:05 +0530

    feat: changes theme and options

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,6 +47,10 @@ return packer.startup(function(use)
   use 'vim-airline/vim-airline'
   use 'vim-airline/vim-airline-themes'
   
+  -- Colorschemes
+  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/darkplus.nvim"
+  
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
