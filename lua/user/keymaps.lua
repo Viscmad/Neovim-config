70f9835a93bb37a8ae43e9452a28dc88b9d9b20d
Vismcad <viscmadsingh93@gmail.com>
2022-07-04 17:17:44 +0530

    fix: fixes semi-colon clash issue

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -24,7 +24,6 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
-keymap("n", ";", ":", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
